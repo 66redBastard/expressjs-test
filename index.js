8532f2da1d6f880e5bd073cb49f16eeb7dafc1a6
redbastard <andrewkrilik@gmail.com>
2021-01-17 20:54:37 +0200

    controllers added / vuejs fetch api data

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import express from "express";
 import path from "path";
 import { requestTime, logger } from "./middleware/middleware.js";
+import serverRoutes from "./routes/server.js";
 
 const __dirname = path.resolve();
 const PORT = process.env.PORT ?? 3000;
 const app = express();
 
-app.set("views", path.resolve(__dirname, "ejs"));
-app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "ejs")); // change path from default dir views to dir ejs
+app.set("view engine", "ejs"); // reset var view engine to ejs
 console.log(app.get("views"));
 
 app.get("/", (req, res) => {
@@ -16,14 +17,12 @@ app.get("/", (req, res) => {
 app.get("/features", (req, res) => {
   res.render("features", { title: "Features Page T", active: "features" });
 });
-app.use(express.static(path.resolve(__dirname, "static")));
 
+// Middleware
+app.use(express.static(path.resolve(__dirname, "static")));
 app.use(requestTime);
 app.use(logger);
-
-// app.get("/download", (req, res) => {
-//   res.download(path.resolve(__dirname, "static", "index.html"));
-// });
+app.use(serverRoutes);
 
 app.listen(PORT, () => {
   console.log(`server has been planted on PORT ${PORT}...`);
