9aefc885122d3c8599886708959ea8d97bf391cc
redbastard <andrewkrilik@gmail.com>
2021-01-17 18:08:51 +0200

    ejs / bootstrap added

--- index.js
+++ index.js
@@ -6,14 +6,24 @@ const __dirname = path.resolve();
 const PORT = process.env.PORT ?? 3000;
 const app = express();
 
+app.set("views", path.resolve(__dirname, "ejs"));
+app.set("view engine", "ejs");
+console.log(app.get("views"));
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Main Page", active: "main" });
+});
+app.get("/features", (req, res) => {
+  res.render("features", { title: "Features Page T", active: "features" });
+});
 app.use(express.static(path.resolve(__dirname, "static")));
 
 app.use(requestTime);
 app.use(logger);
 
-app.get("/download", (req, res) => {
-  res.download(path.resolve(__dirname, "static", "index.html"));
-});
+// app.get("/download", (req, res) => {
+//   res.download(path.resolve(__dirname, "static", "index.html"));
+// });
 
 app.listen(PORT, () => {
   console.log(`server has been planted on PORT ${PORT}...`);
